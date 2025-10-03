84859dc3268ae942a3026dbfcac19cff4114f7dc
Nikolay Fedotov <n.fedotov@emfy.com>
2025-10-03 20:36:19 +0200

    Основные исправления:
    Исправлена опечатка url_seach_param → url_search_param
    
    Убраны проблемные self = this - вместо этого используется const self = this в нужных местах
    
    Добавлена кодировка для значений cookie с помощью encodeURIComponent/decodeURIComponent
    
    Исправлена логика проверки - добавлены правильные проверки на существование элементов
    
    Улучшена обработка ошибок при парсинге URL
    
    Изменен интервал с 100ms на 500ms для снижения нагрузки
    
    Добавлен MutationObserver для отслеживания динамически добавляемых форм
    
    Исправлены проверки на существование элементов и значений
    
    Замена var на const/let для современных стандартов
    
    Добавлена проверка hasOwnProperty при итерации по объекту

--- main.js
+++ main.js
@@ -1,87 +1,140 @@
 class Utm_helper {
-
-  url_seach_param = [
-    'referrer',
-    'utm_content',
-    'utm_medium',
-    'utm_campaign',
-    'utm_source',
-    'utm_term',
-    'utm_referrer',
-    '_ga',
-    '_ym_uid',
-  ];
-  all_utm  = {};
-
   constructor() {
-    self = this;
-    self.url_seach_param.forEach(function(item, index, array) {
-      var get_param = self.getParams(item);
-      var get_cookie =  self.readCookie(item);
-      if(get_param){
-          if(get_param != get_cookie){
-              document.cookie = `${item}=${get_param}; max-age=31536000; path=/`; // пишем метку в куку что бы она не терялась на сайте
-          }
+    this.url_search_param = [
+      'referrer',
+      'utm_content',
+      'utm_medium',
+      'utm_campaign',
+      'utm_source',
+      'utm_term',
+      'utm_referrer',
+      '_ga',
+      '_ym_uid',
+    ];
+    this.all_utm = {};
+    this.init();
+  }
+
+  init() {
+    const self = this;
+    
+    self.url_search_param.forEach(function(item) {
+      const get_param = self.getParams(item);
+      const get_cookie = self.readCookie(item);
+      
+      if (get_param) {
+        if (get_param !== get_cookie) {
+          document.cookie = `${item}=${encodeURIComponent(get_param)}; max-age=31536000; path=/`;
+        }
       }
-      if(get_param || get_cookie){
+      
+      if (get_param || get_cookie) {
         self.all_utm[item] = get_param || get_cookie;
-      }   
+      }
     });
+    
     self.start();
   }
 
-  getParams = function(param) {
-    self = this;
-    if(param == 'referrer'){
-        return document.referrer;
+  getParams(param) {
+    if (param === 'referrer') {
+      return document.referrer || '';
     }
-    const urlParams = new URL(window.location.toString()).searchParams
-    return urlParams.get(param) || false;
+    
+    try {
+      const urlParams = new URL(window.location.toString()).searchParams;
+      const value = urlParams.get(param);
+      return value || '';
+    } catch (e) {
+      console.error('Error parsing URL:', e);
+      return '';
+    }
   }
 
-  readCookie = function(name) {
-    self = this;
-    var name_cook = name+"=";
-    var spl = document.cookie.split(";");
-    for(var i=0; i<spl.length; i++) {
-        var c = spl[i];
-        while(c.charAt(0) == " ") {
-            c = c.substring(1, c.length);
-        }
-        if(c.indexOf(name_cook) == 0) {
-            return c.substring(name_cook.length, c.length);
-        }
+  readCookie(name) {
+    const name_cook = name + "=";
+    const spl = document.cookie.split(";");
+    
+    for (let i = 0; i < spl.length; i++) {
+      let c = spl[i].trim();
+      if (c.indexOf(name_cook) === 0) {
+        return decodeURIComponent(c.substring(name_cook.length, c.length));
+      }
     }
-    return false;
+    return '';
   }
 
-  start = function() {
-    setInterval(function() {
-      var list_form = document.forms;
-      for (var i = 0; i < list_form.length; i++) {
-          for (var key in self.all_utm) {
-              if(!list_form[i].querySelectorAll(`[name="${key}"]`)[0]){
-                  var input = document.createElement('input');
-                  input.type = 'text';
-                  input.className = 'utm_info';
-                  input.style = 'display:none';
-                  input.name = key;
-                  input.value = self.all_utm[key];
-                  list_form[i].appendChild(input);
-              }
+  start() {
+    const self = this;
+    
+    // Функция для добавления UTM полей в формы
+    const addUtmFields = function() {
+      const list_form = document.forms;
+      
+      for (let i = 0; i < list_form.length; i++) {
+        for (const key in self.all_utm) {
+          if (self.all_utm.hasOwnProperty(key) && self.all_utm[key]) {
+            const existingInputs = list_form[i].querySelectorAll(`[name="${key}"]`);
+            if (!existingInputs.length) {
+              const input = document.createElement('input');
+              input.type = 'hidden';
+              input.className = 'utm_info';
+              input.name = key;
+              input.value = self.all_utm[key];
+              list_form[i].appendChild(input);
+            }
           }
+        }
       }
-    }, 100);
-
-    setInterval(function() {
-      var input_utm = document.querySelectorAll('input.utm_info');
-      for (var i = 0; i < input_utm.length; i++) {
-          if(input_utm[i].value == ''){
-              input_utm[i].value = self.all_utm[input_utm[i].name]
+    };
+    
+    // Функция для обновления значений UTM полей
+    const updateUtmFields = function() {
+      const input_utm = document.querySelectorAll('input.utm_info');
+      
+      for (let i = 0; i < input_utm.length; i++) {
+        const fieldName = input_utm[i].name;
+        if ((!input_utm[i].value || input_utm[i].value === '') && self.all_utm[fieldName]) {
+          input_utm[i].value = self.all_utm[fieldName];
+        }
+      }
+    };
+    
+    // Запускаем сразу
+    addUtmFields();
+    updateUtmFields();
+    
+    // Периодически проверяем (реже, чем в оригинале)
+    setInterval(addUtmFields, 500);
+    setInterval(updateUtmFields, 500);
+    
+    // Также добавляем обработчик для динамически добавляемых форм
+    const observer = new MutationObserver(function(mutations) {
+      let shouldCheck = false;
+      
+      for (const mutation of mutations) {
+        if (mutation.type === 'childList') {
+          for (const node of mutation.addedNodes) {
+            if (node.nodeType === 1 && (node.tagName === 'FORM' || node.querySelector('form'))) {
+              shouldCheck = true;
+              break;
+            }
           }
+        }
+        if (shouldCheck) break;
       }
-    }, 100);
+      
+      if (shouldCheck) {
+        setTimeout(addUtmFields, 100);
+      }
+    });
+    
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true
+    });
   }
 }
 
+// Инициализация
 let utm_helper = new Utm_helper();
